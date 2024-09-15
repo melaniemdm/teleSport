b6b9361e21d802c9e56c073ec3471088c196b1b7
melaniemdm <melanieborja.pro@gmail.com>
2024-09-15 18:22:50 +0200

    features : add new components in home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { DisplayTitleComponent } from './features/olympics/components/display-title/display-title.component';
+import { DisplayIndicatorComponent } from './features/olympics/components/display-indicator/display-indicator.component';
 
 @NgModule({
   declarations: [AppComponent, HomeComponent, NotFoundComponent],
-  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, DisplayTitleComponent, DisplayIndicatorComponent],
   providers: [],
   bootstrap: [AppComponent],
 })
