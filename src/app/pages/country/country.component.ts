df3689591911e282babf50806925cf5a985306f8
melaniemdm <melanieborja.pro@gmail.com>
2024-09-17 21:22:16 +0200

    feature: implement olympic services to display value indicator

--- src/app/pages/country/country.component.ts
+++ src/app/pages/country/country.component.ts
@@ -2,6 +2,10 @@ import { Component } from '@angular/core';
 import { DisplayTitleComponent } from "../../features/olympics/components/display-title/display-title.component";
 import { DisplayIndicatorComponent } from "../../features/olympics/components/display-indicator/display-indicator.component";
 import { ActivatedRoute } from '@angular/router';
+import { OlympicService } from 'src/app/core/services/olympic.service';
+import { Country } from 'src/app/core/models/Country';
+
+
 
 @Component({
   selector: 'app-country',
@@ -12,14 +16,25 @@ import { ActivatedRoute } from '@angular/router';
 })
 export class CountryComponent {
   public countryId: string | null = null;  // stores country id
+  public selectedCountry: Country | null = null;
 
-  constructor(private route: ActivatedRoute) {}
+
+  constructor(private route: ActivatedRoute, private olympicService: OlympicService) { }
   ngOnInit(): void {
     //  Get country ID from URL
     this.countryId = this.route.snapshot.paramMap.get('countryId');
     console.log(this.countryId);
-    
-  }
 
+    if (this.countryId) {
+      // Utiliser le service pour obtenir les données du pays sélectionné
+      // console.log(this.olympicService.getOlympicsForCountry(this.countryId).subscribe((countryData: Country | undefined)=>{console.log(countryData)}));
+      // console.log(this.olympicService.getOlympicsForCountry(this.countryId));
+      this.olympicService.getOlympicsForCountry(this.countryId).subscribe((countryData: Country | undefined) => {
+        if (countryData) {
+          // Assigner l'objet complet à `selectedCountry`
+          this.selectedCountry = countryData;
+        }
+      });
+    }
+  }
 }
-// get CountryId from route
