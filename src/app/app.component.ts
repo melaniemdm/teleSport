db07a7e922ab4fcc43ce0824c70735e76fe3e125
melaniemdm <melanieborja.pro@gmail.com>
2024-09-20 17:16:21 +0200

    init ngx-echarts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { take } from 'rxjs';
 import { OlympicService } from './core/services/olympic.service';
+import { provideEcharts } from 'ngx-echarts';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
+  providers: [
+    provideEcharts(),
+  ]
 })
 export class AppComponent implements OnInit {
   constructor(private olympicService: OlympicService) {}
